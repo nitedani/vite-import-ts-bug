c5fed0fb7c74fd831b2b450344201841e92e4b05
Dániel Horváth <horvath.daniel@lexunit.hu>
2022-07-23 23:17:06 +0200

    vps-working

---
+++ apps/app/server/index.ts
@@ -0,0 +1,45 @@
+import express from 'express'
+import compression from 'compression'
+import { renderPage } from 'vite-plugin-ssr'
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const isProduction = process.env.NODE_ENV === 'production'
+const root = `${__dirname}/..`
+
+startServer()
+
+async function startServer() {
+  const app = express()
+
+  app.use(compression())
+
+  if (isProduction) {
+    const sirv = (await import('sirv')).default
+    app.use(sirv(`${root}/dist/client`))
+  } else {
+    const vite = await import('vite')
+    const server = await vite.createServer({
+      root,
+      server: { middlewareMode: true },
+    })
+    app.use(server.middlewares)
+  }
+
+  app.get('*', async (req, res, next) => {
+    const url = req.originalUrl
+    const pageContextInit = {
+      url,
+    }
+    const pageContext = await renderPage(pageContextInit)
+    const { httpResponse } = pageContext
+    if (!httpResponse) return next()
+    const { body, statusCode, contentType } = httpResponse
+    res.status(statusCode).type(contentType).send(body)
+  })
+
+  const port = process.env.PORT || 3000
+  app.listen(port)
+  console.log(`Server running at http://localhost:${port}`)
+}
--- apps/app/src/entry-server.tsx
+++
@@ -1,9 +0,0 @@
-import ReactDOMServer from "react-dom/server";
-import { App } from "./App";
-
-import { myRandom } from "package";
-
-export function render(url, context) {
-  console.log("myRandom()", myRandom());
-  return ReactDOMServer.renderToString(<App />);
-}
