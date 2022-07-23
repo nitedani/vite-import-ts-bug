fa372f51af744117bad300f758b50566eed66e72
Dániel Horváth <horvath.daniel@lexunit.hu>
2022-07-23 23:28:12 +0200

    works

--- apps/app/server/index.ts
+++ apps/app/server/index.ts
@@ -4,7 +4,7 @@ import { renderPage } from 'vite-plugin-ssr'
 import fs from 'fs'
 import path from 'path'
 import { fileURLToPath } from 'url'
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
 const isProduction = process.env.NODE_ENV === 'production'
 const root = `${__dirname}/..`
 
